652bf559edd9b4e8093793ef490b8965a8732028
abdulla <abdullaabdullaev0803@gmail.com>
2023-04-30 08:23:52 +0300

    create a function to place a ship

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ export const Ship = function (length, hitNumber, sunk) {
         length, hitNumber, sunk, hit, isSunk
     }
 }
+const newShip = Ship(3, 0, false)
 
 export const fillArray = function (array, length) {
     for (let i = 0; i < length; i++) {
@@ -19,11 +20,17 @@ export const fillArray = function (array, length) {
     return array
 }
 
-
 export const GameBoard = function (length) {
-    const array = []
-    fillArray(array, length)
-    return { length, array }
+    const map = []
+    fillArray(map, length)
+    function placeShip(row, col) {
+        for (let i = row; i < (row + newShip.length); i++) {
+            map[i][col] = 3
+        }
+    }
+    return { length, map, placeShip }
 }
 const gameBoard = GameBoard(10)
+gameBoard.placeShip(2, 1)
 console.log(gameBoard);
+console.log(gameBoard.map);
--- src/index.test.js
+++ src/index.test.js
@@ -1,4 +1,4 @@
-import { Ship, fillArray } from './index';
+import { Ship, fillArray, GameBoard } from './index';
 
 test('test hitNumber', () => {
     const ship = Ship(1, 0, false)
@@ -10,9 +10,14 @@ test('test sunk', () => {
     const ship = Ship(1, 0, false)
     expect(ship.sunk).toBe(false)
 })
-// test('test array', () => {
-//     const array = []
-//     const result = fillArray(array, 3)
-//     expect(let res).toBe( == 3)
-// })
+test('test fillArray function ', () => {
+    const array = []
+    const result = fillArray(array, 3)
+    expect(result.length).toBe(3)
+})
+test('test fillArray function ', () => {
+    const gameBoard = GameBoard(10)
+
+    expect(gameBoard.placeShip()).toEqual(3)
+})
 
